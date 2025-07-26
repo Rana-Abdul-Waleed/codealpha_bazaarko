9f40057eb947d0faceaa261da31d0e424d88ab0f
Abdul Waleed <abdulwaleedfsd@gmail.com>
2025-07-26 00:21:09 -0700

    created custom error middleware, custom errorHandler function, and verifyToken function for user authorization

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 // import path from "path";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 // import userRoutes from "./routes/user.route.js";
 // import authRoutes from "./routes/auth.route.js";
 
@@ -22,9 +22,9 @@ mongoose
 // express app
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 app.use(cors());
-// app.use(cookieParser());
+app.use(cookieParser());
 
 // run the server
 app.listen(process.env.port, () => {
@@ -42,12 +42,12 @@ app.listen(process.env.port, () => {
 // app.use("/backend/auth", authRoutes);
 
 // custom error middleware
-// app.use((err, req, res, next) => {
-//   const statusCode = err.statusCode || 500;
-//   const message = err.message || "Internal Server Error!";
-//   res.status(statusCode).json({
-//     success: false,
-//     statusCode,
-//     message,
-//   });
-// });
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error!";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
