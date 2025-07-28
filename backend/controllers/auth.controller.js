0bf57172a8cd2684326c7340c9ec830b69391bca
Abdul Waleed <abdulwaleedfsd@gmail.com>
2025-07-27 23:03:29 -0700

    created signin api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -96,51 +96,51 @@ export const signup = async (req, res, next) => {
 };
 
 // signin api
-// export const signin = async (req, res, next) => {
-//   const { email, password } = req.body;
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
 
-//   if (!email || !password || email === "" || password === "") {
-//     return next(errorHandler(400, "All fields are required!"));
-//   }
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required!"));
+  }
 
-//   try {
-//     const validUser = await User.findOne({ email });
-//     if (!validUser) {
-//       return next(errorHandler(404, "User not found!"));
-//     }
-//     const validPassword = bcryptjs.compareSync(password, validUser.password);
-//     if (!validPassword) {
-//       return next(errorHandler(400, "Invalid password!"));
-//     }
-//     const token = jwt.sign(
-//       {
-//         id: validUser._id,
-//         isAdmin: validUser.isAdmin,
-//       },
-//       process.env.JWT_SECRET
-//     );
-//     const { password: pass, ...rest } = validUser._doc;
-//     const safeUser = {
-//       _id: validUser._id,
-//       ...rest,
-//     };
-//     console.log(safeUser);
-//     res.status(200).cookie("access_token", token, { httpOnly: true }).json({
-//       message: "Signin is successful.",
-//       user: safeUser,
-//     });
-//   } catch (error) {
-//     if (error.name === "CastError") {
-//       return next(errorHandler(400, "Invalid data format!"));
-//     }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password!"));
+    }
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+        isAdmin: validUser.isAdmin,
+      },
+      process.env.JWT_SECRET
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    const safeUser = {
+      _id: validUser._id,
+      ...rest,
+    };
+    console.log(safeUser);
+    res.status(200).cookie("access_token", token, { httpOnly: true }).json({
+      message: "Signin is successful.",
+      user: safeUser,
+    });
+  } catch (error) {
+    if (error.name === "CastError") {
+      return next(errorHandler(400, "Invalid data format!"));
+    }
 
-//     if (error.name === "ValidationError") {
-//       const messages = Object.values(error.errors)
-//         .map((val) => val.message)
-//         .join(", ");
-//       return next(errorHandler(400, messages));
-//     }
+    if (error.name === "ValidationError") {
+      const messages = Object.values(error.errors)
+        .map((val) => val.message)
+        .join(", ");
+      return next(errorHandler(400, messages));
+    }
 
-//     next(error);
-//   }
-// };
+    next(error);
+  }
+};
